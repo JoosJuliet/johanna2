0bdef545f2a6ae0c7d1c5bac027398001144786b
JoosJuliet <saven7788@gmail.com>
2020-08-25 23:35:48 +0900

    get setting at the config environment

--- config/env.py
+++ config/env.py
@@ -1,4 +1,5 @@
 import json
+import os
 import sys
 
 config_path = '.'
@@ -7,3 +8,14 @@ if sys.path[0]:
 
 # Load Config.json
 env = json.load(open('./config.json' % config_path))
+with open('./config.json') as config:
+    env = json.load(config.read())
+
+def get_env(setting, env=env):
+    try:
+        return env[setting]
+    except KeyError:
+        error_msg = f'Set the {setting} environment variable'
+
+
+SECRET_KEY = get_env("SECRET_KEY")
--- core/tools.py
+++ core/tools.py
@@ -1,4 +1,31 @@
 from optparse import OptionParser
+import sys
+from config.env import get_env
+
+
+def _confirm_phase():
+    phase = get_env('common')
+    service_name = get_env['common'].get('SERVICE_NAME', '(none)')
+    print('Your current environment values are below')
+    print('-' * 80)
+    print('\tSERVICE_NAME        : %s' % service_name)
+    print('\tPHASE               : %s' % phase)
+    if 'template' in get_env:
+        print('\tTEMPLATE            : %s' % get_env['template']['NAME'])
+    if 'elasticbeanstalk' in get_env:
+        eb = get_env['elasticbeanstalk']
+        for eb_env in eb['ENVIRONMENTS']:
+            aws_default_region = get_env['aws']['AWS_DEFAULT_REGION'] \
+                if 'AWS_DEFAULT_REGION' not in eb_env \
+                else eb_env['AWS_DEFAULT_REGION']
+            print('\tCNAME of %-10s : %-20s (%s)' % (eb_env['NAME'], eb_env['CNAME'], aws_default_region))
+    print('-' * 80)
+
+    answer = input('Please type in the name of phase \'%s\' to confirm: ' % phase)
+    if answer != phase:
+        print('The execution is canceled.')
+        sys.exit(0)
+
 
 def parse_args(require_arg=False):
     if require_arg:
